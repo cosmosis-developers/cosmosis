6f77111368666f4945f05ecf8f09c10b3b0dacc0
andrejdvornik <andrej.dvornik@me.com>
2025-09-24 09:37:21 +0000

    Fix sampler name

--- cosmosis/test/test_samplers.py
+++ cosmosis/test/test_samplers.py
@@ -158,7 +158,7 @@ def test_gridmax():
     run('gridmax', True, can_postprocess=False, max_iterations=1000, hints_cov=False)
 
 def test_latin_hypercube():
-    run('latin_hypercube', True, pp_extra=False, nsample=10, hints_cov=False)
+    run('latinhypercube', True, pp_extra=False, nsample=10, hints_cov=False)
 
 # def test_kombine():
 #     run('kombine')
